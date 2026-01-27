d0a1d6ef66029b75ca7e1d5d70e09f999d315e14
themtaysw <themtaysw@users.noreply.github.com>
2026-01-27 20:49:04 +0100

    babel

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require("eslint/config");
+const { defineConfig, globalIgnores } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
+  globalIgnores(["dist/*"]),
   expoConfig,
   eslintPluginPrettierRecommended,
   {
-    ignores: ["dist/*"],
+    files: ["babel.config.js"],
   },
 ]);
